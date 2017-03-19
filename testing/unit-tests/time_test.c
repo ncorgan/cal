5ac5d0957f85b567f553860842955ab789692c2a
Nicholas Corgan <n.corgan@gmail.com>
2017-03-19 18:32:24 -0500

    time_test: increase cal_usleep time to 500 ms

--- testing/unit-tests/time_test.c
+++ testing/unit-tests/time_test.c
@@ -21,11 +21,12 @@ static void highres_timestamp_test() {
 static void usleep_test() {
     unsigned long long timestamp1 = 0;
     unsigned long long timestamp2 = 0;
+    const unsigned long long sleep_time = 500000; // 500 ms
 
     timestamp1 = cal_highres_timestamp();
-    cal_usleep(5000); // 5 ms
+    cal_usleep(sleep_time);
     timestamp2 = cal_highres_timestamp();
-    TEST_ASSERT_TRUE((timestamp2 - timestamp1) >= 5000);
+    TEST_ASSERT_TRUE((timestamp2 - timestamp1) >= sleep_time);
 }
 
 CAL_TEST_MAIN(
