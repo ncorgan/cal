7fd06461742246742e0243aa1b823d3f908e5098
Nicholas Corgan <n.corgan@gmail.com>
2017-03-26 13:17:28 -0500

    Added cal static library

---
+++ include/cal/net/ifaddrs.h
@@ -0,0 +1,23 @@
+/*!
+ * Copyright (c) 2017 Nicholas Corgan (n.corgan@gmail.com)
+ *
+ * Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+ * or copy at http://opensource.org/licenses/MIT)
+ */
+#ifndef CAL_NET_IFADDRS_H
+#define CAL_NET_IFADDRS_H
+
+#include <cal/config.h>
+
+#include <stdlib.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
