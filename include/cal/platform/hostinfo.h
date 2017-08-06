de2154f454accd85b5f526630d1085a9ad3c537b
Nicholas Corgan <n.corgan@gmail.com>
2017-08-06 03:49:19 -0500

    Added cal_gethostid()

--- include/cal/platform/hostinfo.h
+++ include/cal/platform/hostinfo.h
@@ -19,7 +19,23 @@ extern "C" {
 #    include <unistd.h>
 #endif
 
-static CAL_INLINE int cal_hostname(
+static long cal_gethostid()
+{
+#if defined(CAL_PLATFORM_WIN32) || defined(CAL_PLATFORM_MINGW)
+    // Hopefully it's a safe assumption that a Windows machine has a C:\ drive.
+    char szVolName[MAX_PATH+1], szFileSysName[MAX_PATH+1];
+    DWORD dwSerialNumber, dwMaxComponentLen, dwFileSysFlags;
+    GetVolumeInformation("C:\\", szVolName, MAX_PATH,
+        &dwSerialNumber, &dwMaxComponentLen,
+        &dwFileSysFlags, szFileSysName, sizeof(szFileSysName));
+
+    return (long)dwSerialNumber;
+#else
+    return gethostid();
+#endif
+}
+
+static CAL_INLINE int cal_gethostname(
     char* buffer,
     size_t buffer_len
 )
--- testing/unit-tests/hostinfo_test.c
+++ testing/unit-tests/hostinfo_test.c
@@ -14,13 +14,22 @@
 
 #define BUFFER_LEN 1024
 
-static void hostname_test() {
+static void gethostid_test()
+{
+    long host_id = 0;
+
+    host_id = cal_gethostid();
+    TEST_ASSERT_TRUE(host_id > 0);
+}
+
+static void gethostname_test() {
     char hostname_buffer[BUFFER_LEN] = {0};
-    int error_code = cal_hostname(hostname_buffer, sizeof(hostname_buffer));
+    int error_code = cal_gethostname(hostname_buffer, sizeof(hostname_buffer));
     TEST_ASSERT_EQUAL(0, error_code);
     TEST_ASSERT_TRUE(strlen(hostname_buffer) > 0);
 }
 
 CAL_TEST_MAIN(
-    CAL_TEST(hostname_test)
+    CAL_TEST(gethostid_test)
+    CAL_TEST(gethostname_test)
 )
